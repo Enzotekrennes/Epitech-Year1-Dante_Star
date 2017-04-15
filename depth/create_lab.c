3688786dd875bcf1aeec44eaaecf2cda2cd67dd0
FrostRing <alexandre1.lefevre@epitech.eu>
2017-04-15 18:56:55 +0200

    Moving the solve folder to being the depth folder

---
+++ depth/create_lab.c
@@ -0,0 +1,113 @@
+/*
+** create_lab.c for create map in /home/ludovic.porokhov/delivery/Sem 2/prog/Epitech-Year1-Dante_Star/solve
+** 
+** Made by Ludovic POROKHOV
+** Login   <ludovic.porokhov@epitech.net>
+** 
+** Started on  Wed Apr 12 14:55:08 2017 Ludovic POROKHOV
+<<<<<<< HEAD
+** Last update Thu Apr 13 20:30:39 2017 Ludovic POROKHOV
+=======
+** Last update Thu Apr 13 10:49:51 2017 P3N15
+>>>>>>> 62b1443fd154ff94bc1a379637f0d3594c2e5580
+*/
+
+#include "include/my.h"
+
+char	**make_tab(char *file, int lines)
+{
+  char	**laby;
+  int	i;
+  int	k;
+  int	j;
+  int	len;
+
+  i = 0;
+  j = 0;
+  laby = malloc(sizeof(char *) * (lines + 1));
+  while (file[i])
+    {
+      len = linelen(file + i);
+      laby[j] = malloc(sizeof(char) * len);
+      k = 0;
+      while (file[i] != '\n' && file[i] != '\0')
+	{
+	  laby[j][k] = file[i];
+	  k++;
+	  i++;
+	}
+      laby[j][k] = '\0';
+      i++;
+      j++;
+    }
+  laby[j] = NULL;
+  return (laby);
+}
+
+int	linelen(char *str)
+{
+  int	i;
+
+  i = 0;
+  while (str[i] != '\n')
+    {
+      if (str[i] == '\0')
+	return (i);
+      i++;
+    }
+  return (i);
+}
+
+int	how_many_lines(char *str)
+{
+  int	i;
+  int	j;
+
+  i = 0;
+  j = 1;
+  while (str[i])
+    {
+      if (str[i] == '\n')
+	j++;
+      i++;
+    }
+  return (j);
+}
+
+char    *file_reader(char *filename)
+{
+  struct stat	stats;
+  char  *file;
+  int   fd;
+  int   gotten;
+  
+  stat(filename, &stats);
+  printf("size: %i |", stats.st_size);
+  if ((fd = open(filename, O_RDONLY)) == -1)
+    return (NULL);
+<<<<<<< HEAD
+  if ((file = malloc(sizeof(char) * stats.st_size)) == NULL)
+    return (NULL);
+  while ((gotten = read(fd, file, stats.st_size)) > 0)
+=======
+  if ((file = malloc(10000)) == NULL)
+    return (NULL);
+  while ((gotten = read(fd, file, 10000)) > 0)
+>>>>>>> 62b1443fd154ff94bc1a379637f0d3594c2e5580
+    file[gotten] = '\0';
+  return (file);
+}
+
+char    **labyrinthe(char *filename)
+{
+  char  **laby;
+  char  *file;
+  int   lines;
+  int   i;
+
+  if ((file = file_reader(filename)) == NULL)
+    return (NULL);
+  lines = how_many_lines(file);
+  laby = make_tab(file, lines);
+  return (laby);
+}
---
+++ depth/s_main.c
@@ -0,0 +1,66 @@
+/*
+** s_main.c for main in /home/ludovic.porokhov/delivery/Sem 2/prog/Epitech-Year1-Dante_Star/solve
+** 
+** Made by Ludovic POROKHOV
+** Login   <ludovic.porokhov@epitech.net>
+** 
+** Started on  Tue Apr 11 16:13:59 2017 Ludovic POROKHOV
+<<<<<<< HEAD
+** Last update Thu Apr 13 20:37:31 2017 Ludovic POROKHOV
+=======
+** Last update Thu Apr 13 10:50:21 2017 P3N15
+>>>>>>> 62b1443fd154ff94bc1a379637f0d3594c2e5580
+*/
+
+#include "include/my.h"
+
+void	solver(t_laby ia)
+{
+  int	line;
+  int	col;
+
+  line = tab_line(ia.lab);
+  col = linelen(ia.lab[0]);
+  printf("%i %i\n", line, col);
+  if ((ia.lab = solving(ia, col, line)) == NULL)
+    {
+      printf("no solution found\n");
+      return ;
+    }
+  my_putlab(ia.lab);
+  my_putstr("\n");
+  my_puttab(ia.lab);
+}
+
+char	**solving(t_laby ia, int xmax, int ymax)
+{
+  int	check;
+  
+  ia.x = 0;
+  ia.y = 0;
+  ia.lab[ia.y][ia.x] = 'o';
+  while (ia.x != xmax - 1 || ia.y != ymax - 1)
+    {
+      check = checkmove(ia, xmax, ymax);
+      if (check == 0)
+	ia = forward(ia, xmax, ymax);
+      else if (check == 84)
+	ia = backward(ia, xmax, ymax);
+      else
+	return (NULL);
+      ia.lab[ia.y][ia.x] = 'o';
+    }
+  return (ia.lab);
+}
+
+int	main(int ac, char **av)
+{
+  t_laby	ia;
+
+  if (ac != 2)
+    return (84);
+  if ((ia.lab = labyrinthe(av[1])) == NULL)
+    return (84);
+  solver(ia);
+  return (0);
+}
