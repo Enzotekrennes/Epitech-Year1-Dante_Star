a26951ef147e1a3dbf56f537e52508a093135822
FrostRing <alexandre1.lefevre@epitech.eu>
2017-04-17 17:32:46 +0200

    yes

--- breadth/maze_breadth.c
+++ breadth/maze_breadth.c
@@ -5,20 +5,52 @@
 ** Login   <alexandre1.lefevre@epitech.eu>
 ** 
 ** Started on  Sat Apr 15 17:36:38 2017 P3N15
-** Last update Sat Apr 15 17:53:50 2017 P3N15
+** Last update Mon Apr 17 16:22:24 2017 P3N15
 */
 
 #include "include/my.h"
 
-int	maze_breadth_solver(char **maze, int x, int y)
+int	separate_path(char **maze, int i, int n)
 {
-  int	i;
-  int	n;
+  pid_t	pid = fork();
+  int	dir1;
+  int	dir2;
 
-  i = 0;
-  n = 0;
+  dir1 = first_path(maze, i, n);
+  dir2 = second_path(maze, i, n, dir1);
+  if (pid == 0)
+    {
+      (dir1 == 0) ? n-- : 0;
+      (dir1 == 1) ? i-- : 0;
+      (dir1 == 2) ? i++ : 0;
+      (dir1 == 3) ? n++ : 0;
+    }
+  else if (pid > 0)
+    {
+      (dir2 == 0) ? n-- : 0;
+      (dir2 == 1) ? i-- : 0;
+      (dir2 == 2) ? i++ : 0;
+      (dir2 == 3) ? n++ : 0;
+    } 
+  maze[n][i] = 'o';
+  maze_breadth_solver(maze, i, n);
+  return (0);
+}
+
+int	maze_breadth_solver(char **maze, int i, int n)
+{
+  int	x;
+  int	y;
+
+  x = get_maze_x(maze);
+  y = get_maze_y(maze);
   while (i != x || n != y)
     {
+      if (multiple_path(maze, i, n) != 0)
+	{
+	  separate_path(maze, i, n);
+	  exit(0);
+	}
       if (i != 0 && maze[n][i - 1] == '*')
 	i--;
       else if (i != x && maze[n][i + 1] == '*')
@@ -28,7 +60,42 @@ int	maze_breadth_solver(char **maze, int x, int y)
       else if (n != y && maze[n + 1][i] == '*')
 	n++;
       maze[n][i] = 'o';
-      printf("%d,%d   %d,%d\n", i, n, x, y);
     }
+  my_puttab(maze);
   return (0);
+}
+
+int	first_path(char **maze, int i, int n)
+{
+  int	x;
+  int	y;
+
+  x = get_maze_x(maze);
+  y = get_maze_y(maze);
+  if (n != 0 && maze[n - 1][i] == '*')
+    return (0);
+  if (i != 0 && maze[n][i - 1] == '*')
+    return (1);
+  if (i != x && maze[n][i + 1] == '*')
+    return (2);
+  if (n != y && maze[n + 1][i] == '*')
+    return (3);
+}
+
+int	second_path(char **maze, int i, int n, int dir1)
+{
+  int	x;
+  int	y;
+
+  x = get_maze_x(maze);
+  y = get_maze_y(maze);
+  if (n != 0 && maze[n - 1][i] == '*' && dir1 != 0)
+    return (0);
+  if (i != 0 && maze[n][i - 1] == '*' && dir1 != 1)
+    return (1);
+  if (i != x && maze[n][i + 1] == '*' && dir1 != 2)
+    return (2);
+  if (n != y && maze[n + 1][i] == '*' && dir1 != 3)
+    return (3);
+  
 }
--- breadth/maze_creator.c
+++ breadth/maze_creator.c
@@ -5,7 +5,7 @@
 ** Login   <alexandre1.lefevre@epitech.eu>
 ** 
 ** Started on  Sat Apr 15 17:19:16 2017 P3N15
-** Last update Sat Apr 15 17:52:03 2017 P3N15
+** Last update Mon Apr 17 10:47:14 2017 P3N15
 */
 
 #include "include/my.h"
@@ -78,4 +78,22 @@ int	get_maze_y(char **maze)
   while (maze[y] != '\0')
     y++;
   return (y - 1);
+}
+
+int	mutiple_path(char **maze, int i, int n)
+{
+  int	k;
+
+  k = 0;
+  if (i != 0 && maze[n][i - 1] == '*')
+    k++;
+  if (i != x && maze[n][i + 1] == '*')
+    k++;
+  if (n != 0 && maze[n - 1][i] == '*')
+    k++;
+  if (n != y && maze[n + 1][i] == '*')
+    k++;
+  if (k >= 2)
+    return (1);
+  return (0);
 }
