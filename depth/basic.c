e4c3beb3b46f408e070affb22f98da727def180c
FrostRing <alexandre1.lefevre@epitech.eu>
2017-04-17 17:24:49 +0200

    it should work

---
+++ depth/basic.c
@@ -0,0 +1,79 @@
+/*
+** basic.c for sic in /home/ludovic.porokhov/delivery/Sem 2/prog/Epitech-Year1-Dante_Star/solve
+** 
+** Made by Ludovic POROKHOV
+** Login   <ludovic.porokhov@epitech.net>
+** 
+** Started on  Tue Apr 11 16:13:49 2017 Ludovic POROKHOV
+** Last update Mon Apr 17 17:03:42 2017 Ludovic POROKHOV
+*/
+
+#include "include/my.h"
+
+void	my_putstr(char *str)
+{
+  write(1, str, strlen(str));
+}
+
+void	my_puttab(char **tab)
+{
+  int	i;
+
+  i = 0;
+  while (tab[i])
+    {
+      my_putstr(tab[i++]);
+      my_putstr("\n");
+    }
+}
+
+void	my_putlab(char **tab)
+{
+  int	i;
+  int	j;
+
+  i = 0;
+  while (tab[i])
+    {
+      j = 0;
+      while (tab[i][j])
+	{
+	  if (tab[i][j] == 'D')
+	    printf("\x1B[33m█\x1B[0m");
+	  else if (tab[i][j] == 'o')
+	    printf("\x1B[35m█\x1B[0m");
+	  else if (tab[i][j] == '*')
+	    printf("\x1B[32m█\x1B[0m");
+	  else if (tab[i][j] == 'X')
+	    printf("\x1B[34m█\x1B[0m");
+	  j++;
+	}
+      printf("\n");
+      i++;
+    }
+}
+
+int     howmanylines(char *str)
+{
+  int   i;
+  int   j;
+
+  i = 0;
+  j = 0;
+  while (str[i])
+    {
+      if (str[i] == '\n')
+	j++;;
+      i++;
+    }
+}
+
+int	tab_line(char **tab)
+{
+  int	i;
+
+  i = 0;
+  while (tab[i] != NULL)
+    i++;
+  return (i);
+}
