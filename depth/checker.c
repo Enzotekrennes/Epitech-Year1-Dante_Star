aa2dd981f660ed8d2a0c5704fbf62f3d0dc5089a
Ludovic POROKHOV <ludovic.porokhov@epitech.eu>
2017-05-11 19:35:19 +0200

    depth fini

--- depth/checker.c
+++ depth/checker.c
@@ -5,66 +5,35 @@
 ** Login   <ludovic.porokhov@epitech.net>
 ** 
 ** Started on  Tue May  9 14:08:57 2017 Ludovic POROKHOV
-** Last update Tue May  9 15:10:48 2017 Ludovic POROKHOV
+** Last update Thu May 11 19:28:20 2017 Ludovic POROKHOV
 */
 
 #include "include/my.h"
 
 int     checkmove(t_laby ia, int xmax, int ymax)
 {
-  if (ia.y < ymax - 1)
-    if (ia.lab[ia.y + 1][ia.x] == '*')
-      return (0);
-  if (ia.y > 0)
-    if (ia.lab[ia.y - 1][ia.x] == '*')
-      return (0);
-  if (ia.x < xmax - 1)
-    if (ia.lab[ia.y][ia.x + 1] == '*')
-      return (0);
-  if (ia.x > 0)
-    if (ia.lab[ia.y][ia.x - 1] == '*')
-      return (0);
+  if (ia.y < ymax - 1 && ia.lab[ia.y + 1][ia.x] == '*')
+    return (0);
+  else if (ia.y > 0 && ia.lab[ia.y - 1][ia.x] == '*')
+    return (0);
+  else if (ia.x < xmax - 1 && ia.lab[ia.y][ia.x + 1] == '*')
+    return (0);
+  else if (ia.x > 0 && ia.lab[ia.y][ia.x - 1] == '*')
+    return (0);
   if (checkother(ia, xmax, ymax) == 0)
     return (84);
-  //  if (check_loop(ia, xmax, ymax) == 0)
-  //return (42);
   return (1);
 }
 
 int     checkother(t_laby ia, int xmax, int ymax)
 {
-  if (ia.y < ymax - 1)
-    if (ia.lab[ia.y + 1][ia.x] == 'o')
-      return (0);
-  if (ia.y > 0)
-    if (ia.lab[ia.y - 1][ia.x] == 'o')
-      return (0);
-  if (ia.x < xmax - 1)
-    if (ia.lab[ia.y][ia.x + 1] == 'o')
-      return (0);
-  if (ia.x > 0)
-    if (ia.lab[ia.y][ia.x - 1] == 'o')
-      return (0);
+  if (ia.y < ymax - 1 && ia.lab[ia.y + 1][ia.x] == 'o')
+    return (0);
+  if (ia.y > 0 && ia.lab[ia.y - 1][ia.x] == 'o')
+    return (0);
+  if (ia.x < xmax - 1 && ia.lab[ia.y][ia.x + 1] == 'o')
+    return (0);
+  if (ia.x > 0 && ia.lab[ia.y][ia.x - 1] == 'o')
+    return (0);
   return (84);
 }
-
-/* int	check_loop(t_laby ia, int xmax, int ymax) */
-/* { */
-/*   if (ia.y < ymax - 1) */
-/*     if (ia.lab[ia.y + 1][ia.x] == 'D') */
-/*       return (check_block()); */
-/*   if (ia.y > 0) */
-/*     if (ia.lab[ia.y - 1][ia.x] == 'D') */
-/*       return (0); */
-/*   if (ia.x < xmax - 1) */
-/*     if (ia.lab[ia.y][ia.x + 1] == 'D') */
-/*       return (0); */
-/*   if (ia.x > 0) */
-/*     if (ia.lab[ia.y][ia.x - 1] == 'D') */
-/*       return (0); */
-/* } */
-
-/* int	check_block() */
-/* { */
-/*   return (0); */
-/* } */
--- depth/solver.c
+++ depth/solver.c
@@ -5,64 +5,35 @@
 ** Login   <ludovic.porokhov@epitech.net>
 ** 
 ** Started on  Wed Apr 12 16:52:23 2017 Ludovic POROKHOV
-** Last update Tue May  9 14:09:43 2017 Ludovic POROKHOV
+** Last update Thu May 11 19:22:49 2017 Ludovic POROKHOV
 */
 
 #include "include/my.h"
 
 t_laby	forward(t_laby ia, int xmax, int ymax)
 {
-  if (ia.x < xmax - 1)
-    if (ia.lab[ia.y][ia.x + 1] == '*')
-      {
-	ia.x++;
-	return (ia);
-      }
-  if (ia.y < ymax - 1)
-    if (ia.lab[ia.y + 1][ia.x] == '*')
-      {
-	ia.y++;
-	return (ia);
-      }
-  if (ia.y > 0)
-    if (ia.lab[ia.y - 1][ia.x] == '*')
-      {
-	ia.y--;
-	return (ia);
-      }
-  if (ia.x > 0)
-    if (ia.lab[ia.y][ia.x - 1] == '*')
-      {
-	ia.x--;
-	return (ia);
-      }
+  if (ia.x < xmax - 1 && ia.lab[ia.y][ia.x + 1] == '*')
+    ia.x++;
+  else if (ia.y < ymax - 1 && ia.lab[ia.y + 1][ia.x] == '*')
+    ia.y++;
+  else if (ia.y > 0 && ia.lab[ia.y - 1][ia.x] == '*')
+    ia.y--;
+  else if (ia.x > 0 && ia.lab[ia.y][ia.x - 1] == '*')
+    ia.x--;
+  return (ia);
 }
 
 t_laby	backward(t_laby ia, int xmax, int ymax)
 {
   ia.lab[ia.y][ia.x] = 'D';
-  if (ia.y < ymax - 1)
-    if (ia.lab[ia.y + 1][ia.x] == 'o')
-      {
-	ia.y++;
-	return (ia);
-      }
-  if (ia.x < xmax - 1)
-    if (ia.lab[ia.y][ia.x + 1] == 'o')
-      {
-	ia.x++;
-	return (ia);
-      }
-  if (ia.y > 0)
-    if (ia.lab[ia.y - 1][ia.x] == 'o')
-      {
-	ia.y--;
-	return (ia);
-      }
-  if (ia.x > 0)
-    if (ia.lab[ia.y][ia.x - 1] == 'o')
-      {
-	ia.x--;
-	return (ia);
-      }
+  if (ia.y < ymax - 1 && ia.lab[ia.y + 1][ia.x] == 'o')
+    ia.y++;
+  else if (ia.x < xmax - 1 && ia.lab[ia.y][ia.x + 1] == 'o')
+    ia.x++;
+  else if (ia.y > 0 && ia.lab[ia.y - 1][ia.x] == 'o')
+    ia.y--;
+  else if (ia.x > 0 && ia.lab[ia.y][ia.x - 1] == 'o')
+    ia.x--;
+  return (ia);
 }
+
